cf2c5b673dff5d4c20586722080a926355d7fbdd
Nishan <arifuzzamannishan@gmail.com>
2021-03-24 01:14:27 +0600

    first node code

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require('express'); //for import
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+const users = ['nishan','shathi','anvi','babusona'];
+
 app.get('/',(req,res) => {
-    res.send("nishan");
+    res.send("Arifuzzaman nishan");
+})
+
+app.get('/users/:id',(req,res) => {
+    const id = req.params.id;
+    // console.log(req.query);
+    const name = users[id];
+    res.send({id,name});
+})
+
+app.post('/addUser',(req,res) => {
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
